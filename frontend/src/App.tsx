2d7d2b81b070f088cbc681f3163589012021b012
ShabnamPathan786 <sp83348899@gmail.com>
2026-08-02 23:54:34 +0530

    Restructure frontend: add navbar, home page, replace assets

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
-import "./App.css"
+
+import Home from "./pages/Home"
+
+
 function App() {
 
 
   return (
     <>
-    <h1 className="text-red-500" >Get started</h1>
-</>
+    <Home/>
+    </>
   );
 }
 export default App
